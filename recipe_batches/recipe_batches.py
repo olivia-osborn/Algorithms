51921cbdc4ea7834841f3def148b5c73de3e3cde
Olivia Osborn <olioz95@gmail.com>
2019-05-01 19:07:28 -0400

    wrote pseudocode for recipe batches

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -2,13 +2,20 @@
 
 import math
 
+
 def recipe_batches(recipe, ingredients):
-  pass 
+    least_batches = None
+    # iterate over the recipe
+    # for each corresponding ingredient, divide: recipe/ingredients (rounded down)
+    # If least_batches = None, set first division to this variable
+    # Elif: if the division is less than the least_batches amount, replace least_batches with that new amount
+    # return least_batches
 
 
 if __name__ == '__main__':
-  # Change the entries of these dictionaries to test 
-  # your implementation with different inputs
-  recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
-  print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
+    # Change the entries of these dictionaries to test
+    # your implementation with different inputs
+    recipe = {'milk': 100, 'butter': 50, 'flour': 5}
+    ingredients = {'milk': 132, 'butter': 48, 'flour': 51}
+    print("{batches} batches can be made from the available ingredients: {ingredients}.".format(
+        batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
